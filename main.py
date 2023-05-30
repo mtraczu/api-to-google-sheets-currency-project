7d0fe78f84421389375684e0f0faa5bcc5d74cac
mtraczu <Mikolaj.traczuk@gmail.com>
2023-05-30 19:05:43 +0200

    add API GET endpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-
+from datetime import datetime
+import pytz
+import requests
 # Environment Variables
 
 API_KEY= os.environ["API_KEY"]
@@ -7,7 +9,19 @@ API_GET_ENDPOINT = os.environ["API_GET_ENDPOINT"]
 API_POST_ADDRESS = os.environ["SHEET_POST_ADDRESS"]
 SHEET_NAME = os.environ["SHEET_NAME"]
 
-print(API_KEY)
-print(API_GET_ENDPOINT)
-print(API_POST_ADDRESS)
-print(SHEET_NAME)
+
+date_time = datetime.now(pytz.timezone('GMT')).strftime(f"%Y-%m-%d-%H:%M")
+today_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
+params = {
+    "currency": "EURUSD",
+    "date_time": date_time,
+    "api_key": API_KEY
+}
+
+response = requests.get(API_GET_ENDPOINT, params=params)
+response.raise_for_status()
+data = response.json()
+
+print(data)
